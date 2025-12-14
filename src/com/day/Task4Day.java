d195e120f091ad18457a22d16f89d6e5bdb45aed
Lenovo <Lenovo@DESKTOP-PNAR7AQ>
2025-12-14 12:20:46 +0530

    Week Day Checker program in java

---
+++ src/com/day/Task4Day.java
@@ -0,0 +1,44 @@
+package com.day;
+
+import java.util.Scanner;
+
+public class Task4Day {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+	    int dayNum;
+	    System.out.print("Enter a day number (1-7): ");
+	    dayNum = in.nextInt();
+	    switch(dayNum) {
+	      case 1:
+	        System.out.println("Monday");
+	        break;
+	      case 2:
+	        System.out.println("Tuesday");
+	        break;
+	      case 3:
+	        System.out.println("Wednesday");
+	        break;
+	      case 4:
+	        System.out.println("Thursday");
+	        break;
+	      case 5:
+	        System.out.println("Friday");
+	        System.out.println("TGIF!");
+	        break;
+	      case 6:
+	        System.out.println("Saturday");
+	        break;
+	      case 7:
+	        System.out.println("Sunday");
+	        break;       
+	      default:
+	        System.out.println("Invalid day number.");
+	        break;                      
+	    } 
+	  }
+
+	}
+
+
